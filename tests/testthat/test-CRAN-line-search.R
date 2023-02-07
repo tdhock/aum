73bb22257eb6eda1498fbdc87a13734fdba5e0cf
Toby Dylan Hocking <tdhock5@gmail.com>
2023-02-07 00:29:31 -0700

    four ok

--- tests/testthat/test-CRAN-line-search.R
+++ tests/testthat/test-CRAN-line-search.R
@@ -28,7 +28,7 @@ test_that("line search initial auc correct no tie", {
     data.frame(aum=0, aum.slope.after=0, step.size=2, auc=0.5, auc.after=1)))
 })
 
-test_that("contrived tie computed ok", {
+test_that("contrived three way tie computed ok", {
   three.intersect <- data.frame(
     intercept=c(-1,0,1), 
     slope=c(1, 0, -1),
@@ -39,5 +39,20 @@ test_that("contrived tie computed ok", {
   (expected.df <- rbind(
     data.frame(aum=1, aum.slope.after=-1, step.size=0, auc=3/8, auc.after=3/8),
     data.frame(aum=0, aum.slope.after=0.5, step.size=1, auc=0.5, auc.after=5/8)))
+  expect_equal(L, expected.df)
+})
+
+test_that("binary four way tie computed ok", {
+  four.intersect <- data.frame(
+    intercept=c(-1,1,3,5), 
+    slope=c(1,-1,1,-1),
+    fp.diff=c(0.5,0,0.5,0), 
+    fn.diff=c(0,-0.5,0,-0.5))
+  L <- aum:::aumLineSearch(
+    four.intersect, initialAum = 3, maxIterations = 3)
+  (expected.df <- rbind(
+    data.frame(aum=3, aum.slope.after=-1, step.size=0, auc=1/4, auc.after=1/4),
+    data.frame(aum=2, aum.slope.after=-1, step.size=1, auc=1/2, auc.after=3/4),
+    data.frame(aum=0, aum.slope.after=0, step.size=3, auc=7/8, auc.after=1)))
   expect_equal(L, expected.df)
 })
